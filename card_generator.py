e395efdf808dc2ba68129d889cee0d0547fa7b67
djco21 <danilo.coutinho.431@gmail.com>
2024-12-19 16:56:59 -0300

    quase la

--- card_generator.py
+++ card_generator.py
@@ -1,3 +1,28 @@
 import pygame
-import source
+from random import randint
+
+pygame.init()
+display = pygame.display.set_mode((834, 1080), pygame.FULLSCREEN)
+card = pygame.image.load("assets/card.png")
+font = pygame.font.SysFont('arial black', 45)
+chosen_num = []
+class Random_num(object):
+    def __init__(self, number, posx, posy):
+        self.number = number
+        self.x = posx
+        self.y = posy
+
+def construct_card(chosen_num):
+    for i in range(5):
+        for j in range(5):
+            if i*j != 9:
+                chosen_num.append(Random_num(randint,119+119*j,324+108*i))
+
+def screen():
+    display.blit(card,(0,0))
+clock = pygame.time.Clock()
+FPS = 60
+clock.tick(FPS)
+
+
 
